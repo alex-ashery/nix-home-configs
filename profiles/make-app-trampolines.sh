fb7bfdc3ed2b9f3ed2a2e028373b1a6cf2f3aa56
alex-ashery <alexander.ashery@gmail.com>
2026-09-10 14:38:08 -0700

    fix: app trampoline broke after refactor

--- profiles/make-app-trampolines.sh
+++ profiles/make-app-trampolines.sh
@@ -2,17 +2,39 @@
 # That author wrote https://github.com/hraban/mac-app-util.
 # App linking based on:
 # https://github.com/nix-community/home-manager/issues/1341#issuecomment-1870352014
-fromDir="$HOME/.nix-profile/Applications/Home Manager Apps"
-toDir="$HOME/Applications/Home Manager Apps"
+fromDir="$HOME/Applications/Home Manager Apps"
+toDir="$HOME/Applications/Home Manager Trampolines"
 mkdir -p "$toDir"
 
-find "$toDir" -type l -delete
-
 if [ -d "$fromDir" ]; then
-  find "$fromDir" -maxdepth 1 -type l | while read -r app; do
-    target="$(readlink "$app")"
+  find -L "$fromDir" -maxdepth 1 -type d -name '*.app' | while read -r app; do
     appName="$(basename "$app")"
+    bundleName="${appName%.app}"
+    bundleIdPart="$(printf '%s' "$bundleName" | tr -cs '[:alnum:]' '-' | tr '[:upper:]' '[:lower:]' | sed 's/^-//; s/-$//')"
     trampoline="$toDir/$appName"
-    /usr/bin/osacompile -o "$trampoline" -e "do shell script \"open '$target'\""
+    rm -rf "$trampoline"
+    /usr/bin/osacompile -o "$trampoline" -e "do shell script \"open '$app'\""
+    /usr/libexec/PlistBuddy -c "Add :CFBundleIdentifier string org.home-manager.trampoline.$bundleIdPart" "$trampoline/Contents/Info.plist"
+
+    # Just clobber the applet icon laid down by osacompile rather than do
+    # surgery on the plist.
+    icon="$(find "$app/Contents/Resources" -maxdepth 1 -name '*.icns' -print -quit)"
+    if [ -n "$icon" ]; then
+      cp "$icon" "$trampoline/Contents/Resources/applet.icns"
+    fi
+
+    # Replacing applet.icns mutates a sealed resource in the applet bundle that
+    # osacompile just signed. Re-sign so LaunchServices/Spotlight can trust the
+    # bundle metadata and display the copied icon.
+    /usr/bin/codesign --force --deep --sign - "$trampoline"
+    touch "$trampoline"
+  done
+
+  # Cleanup trampolines for apps Home Manager no longer exposes.
+  find "$toDir" -maxdepth 1 -type d -name '*.app' | while read -r app; do
+    appName="$(basename "$app")"
+    if [ ! -d "$fromDir/$appName" ]; then
+      rm -rf "$app"
+    fi
   done
 fi
